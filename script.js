1ff3b60398e8ca90bed18a2e177a2796d1aadc8a
miftt <miftahfauzi012@gmail.com>
2023-01-10 02:48:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+window.$crisp = [];
+window.CRISP_WEBSITE_ID = "d458c4f5-1c12-4517-92f6-4aa75bee6f63";
+(function () {
+  d = document;
+  s = d.createElement("script");
+  s.src = "https://client.crisp.chat/l.js";
+  s.async = 1;
+  d.getElementsByTagName("head")[0].appendChild(s);
+})();
